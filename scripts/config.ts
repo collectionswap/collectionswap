105c8a0b3d496eb6a175602a7d11e96ed2d14a85
Evil Genius <0xEvilGenius@proton.me>
2022-10-26 16:31:48 +0800

    refactor: clean up deploy script

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,4 +1,5 @@
 type Config = {
+  USE_LEDGER: boolean;
   FACTORY: string;
   EXPORT_FILENAME: string;
 };
@@ -6,12 +7,20 @@ type Config = {
 export const configs: { [key: number]: Config } = {
   // Mainnet
   1: {
+    USE_LEDGER: true,
     FACTORY: "0xb16c1342E617A5B6E4b631EB114483FDB289c0A4",
     EXPORT_FILENAME: "mainnetAddresses.json",
   },
   // Rinkeby
   4: {
+    USE_LEDGER: false,
     FACTORY: "0xcB1514FE29db064fa595628E0BFFD10cdf998F33",
     EXPORT_FILENAME: "rinkebyAddresses.json",
+  },
+  // Hardhat
+  31337: {
+    USE_LEDGER: false,
+    FACTORY: "",
+    EXPORT_FILENAME: "hardhatAddresses.json",
   },
 };
--- scripts/deployCollectionSwapAndStaker.ts
+++ scripts/deployCollectionSwapAndStaker.ts
@@ -1,107 +1,63 @@
+/* eslint-disable camelcase */
+import fs from "fs";
+
 import { LedgerSigner } from "@anders-t/ethers-ledger";
 
 import { configs } from "./config";
 
 import type {
+  Collectionstaker__factory,
+  Collectionswap__factory,
   Collectionstaker,
   Collectionswap,
-  RewardPoolETH,
 } from "../typechain-types";
 import type { HardhatRuntimeEnvironment } from "hardhat/types";
 
 let collectionSwap: Collectionswap;
 let collectionStaker: Collectionstaker;
-let rewardPool: RewardPoolETH;
 
 export async function deployCollectionSwapAndStaker(
   hre: HardhatRuntimeEnvironment
 ) {
-  const ledger = new LedgerSigner(hre.ethers.provider);
-  // Const ledger = await new LedgerSigner(hre.ethers.provider, "hid", "m/44'/60'/0'/0");
-  const ledgerAddress = await ledger.getAddress();
-  console.log(`Deployer: ${ledgerAddress}`);
-
   const networkId = hre.network.config.chainId as number;
-  console.log("networkId", networkId);
-  // Const [deployer] = await hre.ethers.getSigners();
-  // deployerAddress = await deployer.getAddress();
-  // console.log(`Deployer: ${deployerAddress}`);
+  console.log(`NetworkId: ${networkId}`);
+  const config = configs[networkId];
 
-  const factoryAddress = configs[networkId].FACTORY;
-  // Console.log(`Deploying Collectionswap...`);
-  // let collectionSwapFactory = (await hre.ethers.getContractFactory('Collectionswap')) as Collectionswap__factory;
-  // collectionSwapFactory = await collectionSwapFactory.connect(ledger);
-  // console.log('Deploying collectionswap...')
-  console.log("factoryAddress", factoryAddress);
-  // CollectionSwap = await collectionSwapFactory.deploy(factoryAddress,{gasLimit: 5000000});
-  // console.log('Deployed collectionswap!')
-  // console.log('collectionswap deployed...')
-  // await collectionSwap.deployed();
-  collectionSwap = await hre.ethers.getContractAt(
+  const [deployer] = config.USE_LEDGER
+    ? [new LedgerSigner(hre.ethers.provider)]
+    : await hre.ethers.getSigners();
+  const deployerAddress = await deployer.getAddress();
+  console.log(`Deployer: ${deployerAddress}`);
+
+  const factoryAddress = config.FACTORY;
+  console.log(`Deploying Collectionswap...`);
+  const collectionSwapFactory = (await hre.ethers.getContractFactory(
     "Collectionswap",
-    "0x226620C03C2f2dBBBd90E2Eca4754D8a41Fd3DEB"
-  );
+    deployer
+  )) as Collectionswap__factory;
+  collectionSwap = await collectionSwapFactory.deploy(factoryAddress);
+  await collectionSwap.deployed();
   console.log(`Collectionswap address: ${collectionSwap.address}`);
 
-  console.log("Deploying Collectionstaker...");
-  const collectionStakerFactory = await hre.ethers.getContractFactory(
-    "Collectionstaker"
-  );
-  collectionStakerFactory.connect(ledger);
-  console.log("Deploying Collectionstaker...");
-  console.log("collectionSwap.address", collectionSwap.address);
-  // CollectionStaker = await collectionStakerFactory.deploy(collectionSwap.address,{gasLimit: 6000000});
-  console.log("Deployed collectionstaker!");
-  console.log("collectionstaker deployed...");
-  // Await collectionStaker.deployed();
-
-  collectionStaker = await hre.ethers.getContractAt(
+  console.log(`Deploying Collectionstaker...`);
+  const collectionStakerFactory = (await hre.ethers.getContractFactory(
     "Collectionstaker",
-    "0x52Ef2a32F6db19cB82EFcEb63Ff6476A2645be7f"
+    deployer
+  )) as Collectionstaker__factory;
+  collectionStaker = await collectionStakerFactory.deploy(
+    collectionSwap.address
   );
+  await collectionStaker.deployed();
   console.log(`Collectionstaker address: ${collectionStaker.address}`);
 
-  // Console.log('exporting addresses...');
-  // let addressesToExport = {
-  //     // deployer: deployerAddress,
-  //     deployer: ledgerAddress,
-  //     collectionSwap: collectionSwap.address,
-  //     collectionStaker: collectionStaker.address
-  // };
-  // let exportJson = JSON.stringify(addressesToExport, null, 2);
-  // fs.writeFileSync(configs[networkId].EXPORT_FILENAME, exportJson);
-
-  // collectionStaker = (await hre.ethers.getContractAt('Collectionstaker', '0x4aa8ae968d01a8e0335ecb819c802b94c0398690'));
-  // 0x4aa8ae968d01a8e0335ecb819c802b94c0398690
-  // console.log(`Collectionstaker address: ${collectionStaker.address}`);
-
-  console.log("Deploying RewardPoolETH...");
-  const nftAddress = collectionSwap.address; // Kludge
-  const curveAddress = "0x432f962D8209781da23fB37b6B59ee15dE7d9841";
-  const delta = hre.ethers.utils.parseEther("1.1");
-  const fee = hre.ethers.utils.parseEther("0.1");
-  const wethArray = ["0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"];
-  const rewardRates = [hre.ethers.utils.parseEther("0.001")];
-  const startTime = 1664165919;
-  const endTime = 1664165920;
-
-  let rewardPoolFactory = await hre.ethers.getContractFactory("RewardPoolETH");
-  rewardPoolFactory = rewardPoolFactory.connect(ledger);
-  rewardPool = await rewardPoolFactory.deploy(
-    ledgerAddress,
-    collectionSwap.address,
-    nftAddress,
-    curveAddress,
-    delta,
-    fee,
-    wethArray,
-    rewardRates,
-    startTime,
-    endTime,
-    { gasLimit: 3000000 }
-  );
-
-  // Let rewardPool = await rewardPoolFactory.deploy(collectionStaker.address, {gasLimit: 6000000});
+  console.log("exporting addresses...");
+  const addressesToExport = {
+    deployer: deployerAddress,
+    collectionSwap: collectionSwap.address,
+    collectionStaker: collectionStaker.address,
+  };
+  const exportJson = JSON.stringify(addressesToExport, null, 2);
+  fs.writeFileSync(config.EXPORT_FILENAME, exportJson);
 
   console.log(
     "waiting for etherscan backend propagation... sleeping for 1 minute"
@@ -113,32 +69,15 @@ export async function deployCollectionSwapAndStaker(
     console.log(`${60 - 15 * i}s left...`);
   }
 
-  // Console.log('verifying Collectionswap...');
-  // await hre.run("verify:verify", {
-  // address: collectionSwap.address,
-  // constructorArguments: [factoryAddress]
-  // });
-
-  // console.log('verifying Collectionstaker...');
-  // await hre.run("verify:verify", {
-  //     address: collectionStaker.address,
-  //     constructorArguments: [collectionSwap.address]
-  // });
-
-  console.log("verifying RewardPoolETH...");
+  console.log("verifying Collectionswap...");
   await hre.run("verify:verify", {
-    address: rewardPool.address,
-    constructorArguments: [
-      ledgerAddress,
-      collectionSwap.address,
-      nftAddress,
-      curveAddress,
-      delta,
-      fee,
-      wethArray,
-      rewardRates,
-      startTime,
-      endTime,
-    ],
+    address: collectionSwap.address,
+    constructorArguments: [factoryAddress],
+  });
+
+  console.log("verifying Collectionstaker...");
+  await hre.run("verify:verify", {
+    address: collectionStaker.address,
+    constructorArguments: [collectionSwap.address],
   });
 }
