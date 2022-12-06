78355431a61b1549a06c5448359533f4cbf18027
Evil Genius <0xEvilGenius@proton.me>
2022-12-06 22:45:22 +0800

    refactor deploy collection set script

--- scripts/deployCollectionSet.ts
+++ scripts/deployCollectionSet.ts
@@ -8,41 +8,11 @@ import { configs } from "./config";
 import type {
   Collectionstaker__factory,
   LSSVMPairFactory__factory,
-  MonotonicIncreasingValidator__factory,
   RNGChainlinkV2__factory,
-  Collectionstaker,
-  LSSVMPairFactory,
-  MonotonicIncreasingValidator,
   RNGChainlinkV2,
 } from "../typechain-types";
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import type { HardhatRuntimeEnvironment } from "hardhat/types";
-
-let deployFactory: any;
-// Templates
-const templateNames = [
-  "LSSVMPairEnumerableETH",
-  "LSSVMPairMissingEnumerableETH",
-  "LSSVMPairEnumerableERC20",
-  "LSSVMPairMissingEnumerableERC20",
-];
-const templateAddresses: string[] = [];
-
-// Curves
-const curveNames = [
-  "LinearCurve",
-  "ExponentialCurve",
-  "XykCurve",
-  "SigmoidCurve",
-];
-const curveAddresses: string[] = [];
-
-// SortitionTree
-let treeAddress: string;
-
-let factory: LSSVMPairFactory;
-let collectionStaker: Collectionstaker;
-let monotonicIncreasingValidator: MonotonicIncreasingValidator;
-let rng: RNGChainlinkV2;
 
 export async function deployCollectionSet(hre: HardhatRuntimeEnvironment) {
   const networkId = hre.network.config.chainId as number;
@@ -55,27 +25,17 @@ export async function deployCollectionSet(hre: HardhatRuntimeEnvironment) {
   const deployerAddress = await deployer.getAddress();
   console.log(`Deployer: ${deployerAddress}`);
 
-  console.log(`---------------------------------`);
-  console.log(`------ Deploying Templates ------`);
-  console.log(`---------------------------------`);
-
-  for (const templateName of templateNames) {
-    console.log(`Deploying ${templateName}`);
-    deployFactory = await hre.ethers.getContractFactory(templateName, deployer);
-    const deployedTemplate = await deployFactory.deploy();
-    await deployedTemplate.deployed();
-    console.log(`${templateName} address: ${deployedTemplate.address}`);
-    templateAddresses.push(deployedTemplate.address);
-  }
-
-  console.log(`------------------------------`);
+  const { templateNames, templateAddresses } = await deployTemplates(
+    hre,
+    deployer
+  );
 
   console.log(`Deploying factory...`);
   const lssvmFactory = (await hre.ethers.getContractFactory(
     "LSSVMPairFactory",
     deployer
   )) as LSSVMPairFactory__factory;
-  factory = await lssvmFactory.deploy(
+  const factory = await lssvmFactory.deploy(
     templateAddresses[0],
     templateAddresses[1],
     templateAddresses[2],
@@ -87,38 +47,14 @@ export async function deployCollectionSet(hre: HardhatRuntimeEnvironment) {
   await factory.deployed();
   console.log(`Factory address: ${factory.address}`);
 
-  console.log(`--------------------------------`);
-  console.log(`------- Deploying Curves -------`);
-  console.log(`--------------------------------`);
+  const { curveNames, curveAddresses } = await deployCurves(hre, deployer);
 
-  for (const curveName of curveNames) {
-    console.log(`Deploying ${curveName}`);
-    deployFactory = await hre.ethers.getContractFactory(curveName, deployer);
-    const deployedCurve = await deployFactory.deploy();
-    await deployedCurve.deployed();
-    console.log(`${curveName} address: ${deployedCurve.address}`);
-    curveAddresses.push(deployedCurve.address);
-
-    console.log(`Whitelisting ${curveName} in factory...`);
-    await factory.setBondingCurveAllowed(deployedCurve.address, true);
+  for (let i = 0; i < curveNames.length; i++) {
+    console.log(`Whitelisting ${curveNames[i]} in factory...`);
+    await factory.setBondingCurveAllowed(curveAddresses[i], true);
   }
 
-  console.log(`------------------------------`);
-
-  console.log(`---------------------------------------`);
-  console.log(`------- Deploying SortitionTree -------`);
-  console.log(`---------------------------------------`);
-
-  deployFactory = await hre.ethers.getContractFactory(
-    "SortitionSumTreeFactory",
-    deployer
-  );
-  const deployedTree = await deployFactory.deploy();
-  await deployedTree.deployed();
-  console.log(`${"SortitionTree"} address: ${deployedTree.address}`);
-  treeAddress = deployedTree.address;
-
-  console.log(`------------------------------`);
+  const treeAddress = await deploySortitionTree(hre, deployer);
 
   console.log(`Deploying Collectionstaker...`);
   const collectionStakerFactory = (await hre.ethers.getContractFactory(
@@ -130,37 +66,13 @@ export async function deployCollectionSet(hre: HardhatRuntimeEnvironment) {
       },
     }
   )) as Collectionstaker__factory;
-  collectionStaker = await collectionStakerFactory.deploy(factory.address);
+  const collectionStaker = await collectionStakerFactory.deploy(factory.address);
   await collectionStaker.deployed();
   console.log(`Collectionstaker address: ${collectionStaker.address}`);
 
-  console.log(`Deploying Monotonically Increasing Validator`);
-  const monotonicIncreasingValidatorFactory =
-    (await hre.ethers.getContractFactory(
-      "MonotonicIncreasingValidator",
-      deployer
-    )) as MonotonicIncreasingValidator__factory;
+  const { validatorAddresses } = await deployValidators(hre, deployer);
 
-  monotonicIncreasingValidator =
-    await monotonicIncreasingValidatorFactory.deploy();
-  await monotonicIncreasingValidator.deployed();
-  console.log(
-    `Monotonic Increasing Validator address: ${monotonicIncreasingValidator.address}`
-  );
-
-  console.log(`Deploying ChainlinkRNGv2...`);
-  const rngFactory = (await hre.ethers.getContractFactory(
-    "RNGChainlinkV2",
-    deployer
-  )) as RNGChainlinkV2__factory;
-  rng = await rngFactory.deploy(
-    deployerAddress,
-    config.VRF_COORDINATOR,
-    config.SUBSCRIPTION_ID,
-    config.KEY_HASH
-  );
-  await rng.deployed();
-  console.log(`Chainlink RNG address: ${collectionStaker.address}`);
+  const rng = await deployChainlink(hre, deployer);
 
   console.log(`Setting RNG in staker...`);
   // Set RNG in staker
@@ -170,6 +82,10 @@ export async function deployCollectionSet(hre: HardhatRuntimeEnvironment) {
   await rng.setAllowedCaller(collectionStaker.address);
 
   console.log("exporting addresses...");
+  const [rewardPoolETHAddress, rewardPoolETHDrawAddress] = await Promise.all([
+    collectionStaker.rewardPoolETHLogic(),
+    collectionStaker.rewardPoolETHDrawLogic(),
+  ]);
   const addressesToExport = {
     deployer: deployerAddress,
     lssvmPairEnumerableETH: templateAddresses[0],
@@ -182,8 +98,10 @@ export async function deployCollectionSet(hre: HardhatRuntimeEnvironment) {
     sigmoidCurve: curveAddresses[3],
     factory: factory.address,
     collectionStaker: collectionStaker.address,
+    rewardPoolETH: rewardPoolETHAddress,
+    rewardPoolETHDraw: rewardPoolETHDrawAddress,
     rng: rng.address,
-    monotonicIncreasingValidator: monotonicIncreasingValidator.address,
+    monotonicIncreasingValidator: validatorAddresses[0],
     tree: treeAddress,
   };
   const exportJson = JSON.stringify(addressesToExport, null, 2);
@@ -247,19 +165,19 @@ export async function deployCollectionSet(hre: HardhatRuntimeEnvironment) {
 
   console.log("verifying RewardETHLogic...");
   await hre.run("verify:verify", {
-    address: await collectionStaker.rewardPoolETHLogic(),
+    address: rewardPoolETHAddress,
     constructorArguments: [],
   });
 
   console.log("verifying RewardETHDrawLogic...");
   await hre.run("verify:verify", {
-    address: await collectionStaker.rewardPoolETHDrawLogic(),
+    address: rewardPoolETHDrawAddress,
     constructorArguments: [],
   });
 
   console.log("verifying Monotonic Increasing Validator...");
   await hre.run("verify:verify", {
-    address: monotonicIncreasingValidator.address,
+    address: validatorAddresses[0],
     constructorArguments: [],
   });
 
@@ -273,4 +191,145 @@ export async function deployCollectionSet(hre: HardhatRuntimeEnvironment) {
       config.KEY_HASH,
     ],
   });
+}
+
+export async function deployTemplates(
+  hre: HardhatRuntimeEnvironment,
+  deployer: LedgerSigner | SignerWithAddress
+): Promise<{ templateNames: string[]; templateAddresses: string[] }> {
+  // Templates
+  const templateNames = [
+    "LSSVMPairEnumerableETH",
+    "LSSVMPairMissingEnumerableETH",
+    "LSSVMPairEnumerableERC20",
+    "LSSVMPairMissingEnumerableERC20",
+  ];
+  const templateAddresses: string[] = [];
+
+  console.log(`---------------------------------`);
+  console.log(`------ Deploying Templates ------`);
+  console.log(`---------------------------------`);
+
+  for (const templateName of templateNames) {
+    console.log(`Deploying ${templateName}`);
+    const deployFactory = await hre.ethers.getContractFactory(
+      templateName,
+      deployer
+    );
+    const deployedTemplate = await deployFactory.deploy();
+    await deployedTemplate.deployed();
+    console.log(`${templateName} address: ${deployedTemplate.address}`);
+    templateAddresses.push(deployedTemplate.address);
+  }
+
+  console.log(`------------------------------`);
+
+  return { templateNames, templateAddresses };
+}
+
+export async function deployCurves(
+  hre: HardhatRuntimeEnvironment,
+  deployer: LedgerSigner | SignerWithAddress
+): Promise<{ curveNames: string[]; curveAddresses: string[] }> {
+  // Curves
+  const curveNames = [
+    "LinearCurve",
+    "ExponentialCurve",
+    "XykCurve",
+    "SigmoidCurve",
+  ];
+  const curveAddresses: string[] = [];
+
+  console.log(`--------------------------------`);
+  console.log(`------- Deploying Curves -------`);
+  console.log(`--------------------------------`);
+
+  for (const curveName of curveNames) {
+    console.log(`Deploying ${curveName}`);
+    const deployFactory = await hre.ethers.getContractFactory(
+      curveName,
+      deployer
+    );
+    const deployedCurve = await deployFactory.deploy();
+    await deployedCurve.deployed();
+    console.log(`${curveName} address: ${deployedCurve.address}`);
+    curveAddresses.push(deployedCurve.address);
+  }
+
+  console.log(`------------------------------`);
+
+  return { curveNames, curveAddresses };
+}
+
+export async function deployValidators(
+  hre: HardhatRuntimeEnvironment,
+  deployer: LedgerSigner | SignerWithAddress
+): Promise<{ validatorNames: string[]; validatorAddresses: string[] }> {
+  // Validators
+  const validatorNames = ["MonotonicIncreasingValidator"];
+  const validatorAddresses: string[] = [];
+
+  console.log(`--------------------------------`);
+  console.log(`------- Deploying Validators -------`);
+  console.log(`--------------------------------`);
+
+  for (const validatorName of validatorNames) {
+    console.log(`Deploying ${validatorName}`);
+    const deployFactory = await hre.ethers.getContractFactory(
+      validatorName,
+      deployer
+    );
+    const deployedCurve = await deployFactory.deploy();
+    await deployedCurve.deployed();
+    console.log(`${validatorName} address: ${deployedCurve.address}`);
+    validatorAddresses.push(deployedCurve.address);
+  }
+
+  console.log(`------------------------------`);
+
+  return { validatorNames, validatorAddresses };
+}
+
+export async function deploySortitionTree(
+  hre: HardhatRuntimeEnvironment,
+  deployer: LedgerSigner | SignerWithAddress
+): Promise<string> {
+  console.log(`---------------------------------------`);
+  console.log(`------- Deploying SortitionTree -------`);
+  console.log(`---------------------------------------`);
+
+  const deployFactory = await hre.ethers.getContractFactory(
+    "SortitionSumTreeFactory",
+    deployer
+  );
+  const deployedTree = await deployFactory.deploy();
+  await deployedTree.deployed();
+  console.log(`SortitionTree address: ${deployedTree.address}`);
+
+  console.log(`------------------------------`);
+
+  return deployedTree.address;
+}
+
+export async function deployChainlink(
+  hre: HardhatRuntimeEnvironment,
+  deployer: LedgerSigner | SignerWithAddress
+): Promise<RNGChainlinkV2> {
+  const config = configs[hre.network.config.chainId!];
+
+  console.log(`Deploying ChainlinkRNGv2...`);
+  const rngFactory = (await hre.ethers.getContractFactory(
+    "RNGChainlinkV2",
+    deployer
+  )) as RNGChainlinkV2__factory;
+  const rng = await rngFactory.deploy(
+    await deployer.getAddress(),
+    config.VRF_COORDINATOR,
+    config.SUBSCRIPTION_ID,
+    config.KEY_HASH
+  );
+  await rng.deployed();
+  console.log(`Chainlink RNG address: ${rng.address}`);
+
+  return rng;
 }
